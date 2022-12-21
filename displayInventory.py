7fab0be5884b85a367c40b3e52ea511c1e7e676a
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-21 13:55:57 -0600

    Update displayInventory.py

--- displayInventory.py
+++ displayInventory.py
@@ -3,4 +3,6 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
     # your code goes here
 
-##displayInventory(stuff)
+
+if __name__ == "__main__":
+    displayInventory(stuff)
